8a6af30ce93ada97c150aa6465c70bb8df659b19
Scott Isaacson <scott@hitechlowlife.com>
2024-11-18 11:42:16 -0500

    edited files

--- src/content/about/index.md
+++ src/content/about/index.md
@@ -1,20 +1,23 @@
 ---
-title: "I’m John Doe, A content writer based in LDN, Currently at Bookworm"
-meta_title: "About"
-image: "/images/author.png"
+title: 'I’m John Doe, A content writer based in LDN, Currently at Bookworm'
+meta_title: About
+image: /images/author.png
 draft: false
-
 what_i_do:
-  title: "What I Do"
+  title: stuff I do
   items:
-    - title: "Content Writing"
-      description: "Purus eget ipsum elementum venenatis, quis rutrum mi semper nonpurus eget ipsum elementum venenatis."
-
-    - title: "Photography"
-      description: "Aenean maximus urna magna elementum, quis rutrum mi semper non purus eget ipsum venenatis."
-
-    - title: "Web Research"
-      description: "Aenean maximus urna magna elementum venenatis, quis semper non purus eget ipsum venenatis."
+    - title: writing
+      description: >-
+        Purus eget ipsum elementum venenatis, quis rutrum mi semper nonpurus
+        eget ipsum elementum venenatis.
+    - title: Photography
+      description: >-
+        Aenean maximus urna magna elementum, quis rutrum mi semper non purus
+        eget ipsum venenatis.
+    - title: Web Research
+      description: >-
+        Aenean maximus urna magna elementum venenatis, quis semper non purus
+        eget ipsum venenatis.
 ---
 
 A content writer with over 12 years experience working across brand identity, publishing and digital products. Maecenas sit amet purus eget ipsum elementum venenatis. Aenean maximus urna magna elementum venenatis quis non purus.
--- tina/config.ts
+++ tina/config.ts
@@ -27,18 +27,145 @@ export default defineConfig({
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
-    collections: [
+    collections: [{
+      label: 'About Page',
+      name: 'About',
+      path: 'src/content/about',
+      format: 'md',
+      fields: [
+        {
+          type: 'string',
+          label: 'Title',
+          name: 'title',
+        },
+        {
+          type: 'string',
+          label: 'Meta-Title',
+          name: 'meta_title',
+        },
+        {
+          type: 'image',
+          label: 'Image',
+          name: 'image',
+          required: true,
+        },
+        {
+          type: 'boolean',
+          name: 'draft',
+          label: 'Keep as Draft?'
+        },
+        {
+          type:'object',
+          name: 'what_i_do',
+          label: 'What I Do',
+          list: false,
+          fields:[{
+            type:'string',
+            name: 'title',
+            label: 'Bottom Section Title'
+          },{
+            name: "items",
+            label: "items",
+            type: "object",
+            list: true,
+            ui: {
+              itemProps: (items) => {
+                return { label: items?.name };
+              },
+            },
+            fields: [
+              {
+                name: "title",
+                label: "Section Title",
+                type: "string",
+                required: true,
+              },
+              {
+                name: "description",
+                label: "Section Description",
+                type: "string",
+                required: true,
+              },
+            ],
+          },]
+        },
+        {
+          type: "rich-text",
+          name: "body",
+          label: "Body",
+          isBody: true,
+        }
+        ],
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false,
+          },
+      },
+    },
+    {
+      label: 'Authors',
+      name: 'Authors',
+      path: 'src/content/authors',
+      format: 'md',
+      fields: [
+        {
+          type: 'string',
+          label: 'Author Name',
+          name: 'title',
+        },
+        {
+          type: 'image',
+          label: 'Image',
+          name: 'image',
+          required: true,
+        },
+        {
+          type: 'string',
+          label: 'About the Author',
+          name: 'description',
+        },
+        {
+          type:'object',
+          name: 'social',
+          label: 'Social Media',
+          list: false,
+          fields:[{
+            type:'string',
+            name: 'facebook',
+            label: 'Facebook URL'
+          },
+          {
+            type:'string',
+            name: 'twitter',
+            label: 'Twitter URL'
+          },
+          {
+            type:'string',
+            name: 'Instagram',
+            label: 'Instagram URL'
+          },
+          ],
+        },
+        {
+          type: "rich-text",
+          name: "body",
+          label: "Body",
+          isBody: true,
+        }
+        ],
+        ui: {
+          allowedActions: {
+            create: true,
+            delete: true,
+          },
+      },
+    },
       {
         name: "post",
         label: "New Post",
         path: "src/content/posts",
         format: "md",
-        ui: {
-          allowedActions: {
-            create: true,
-            delete: true, // Ensure this is set to true
-          },
-        },
         fields: [
           {
             type: "string",
@@ -89,7 +216,8 @@ export default defineConfig({
         ],
       },
     ],
-  },search: {
+  },
+  search: {
     tina: {
       indexerToken: '8f75ddca4f33f04e60abc380cad8c34c8f5c4171',
       stopwordLanguages: ['eng'],
